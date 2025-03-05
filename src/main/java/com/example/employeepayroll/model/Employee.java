fec5e443b92935485498f6f67623530b42182f03
Gajendra_Pratap_Singh <gajendrapratapsingh0405@gmail.com>
2025-03-05 16:12:15 +0530

    [Gajendra] Added: Created Employee Payroll App

---
+++ src/main/java/com/example/employeepayroll/model/Employee.java
@@ -0,0 +1,18 @@
+package com.example.employeepayroll.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String department;
+    private double salary;
+}
